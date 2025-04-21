1d8173fc297e5091483cd476f8e4cd6c975e45f5
coder-dipesh <coderdofficial@gmail.com>
2025-04-21 19:09:56 +1000

    Employee Manager class to manage employee function, Textbase CLI to display user menu, UIEnhancement for colors animation

---
+++ src/TextBasedCLI.java
@@ -0,0 +1,164 @@
+import java.util.List;
+import java.util.Scanner;
+
+
+public class TextBasedCLI {
+    EmployeeManager employeeManager = new EmployeeManager();
+    Scanner sc = new Scanner(System.in);
+    int choice;
+
+
+    public void userMenu() {
+        do {
+            System.out.println(UIEnhancement.CYAN + "\n=============================================================");
+            System.out.println("                 EMPLOYEE MANAGEMENT SYSTEM                 ");
+            System.out.println("=============================================================" + UIEnhancement.RESET);
+
+            System.out.println(UIEnhancement.YELLOW + ">> MENU OPTIONS <<" + UIEnhancement.RESET);
+            System.out.println("1.  Load Employees");
+            System.out.println("2.  View All Employees");
+            System.out.println("3.  Add New Employee");
+            System.out.println("4.  Search by Name");
+            System.out.println("5.  Search by Performance Rating");
+            System.out.println("6.  Search by ID");
+            System.out.println("7.  Update Employee");
+            System.out.println("8.  Delete Employee");
+            System.out.println("9.  View Recent Actions");
+            System.out.println("10. Save Changes");
+            System.out.println("11. Exit");
+
+            // user input validation
+            while (true) {
+                System.out.print("\nEnter your choice (1–11): ");
+                String input = sc.nextLine();
+                try {
+                    choice = Integer.parseInt(input);
+                    if (choice >= 1 && choice <= 11) break;
+                    else System.out.println(UIEnhancement.RED + "❌ Please enter a number between 1 and 11." + UIEnhancement.RESET);
+                } catch (NumberFormatException e) {
+                    System.out.println(UIEnhancement.RED + "❌ Invalid input. Enter a valid number." + UIEnhancement.RESET);
+                }
+            }
+
+            String filePath = "data/employee_data.txt";
+            switch (choice) {
+                case 1 -> {
+                    UIEnhancement.progressBar("\uD83D\uDCC2 Loading Employees Data");
+                    employeeManager.loadFromFile(filePath);
+                }
+
+                case 2 -> employeeManager.viewAllEmployees();
+
+                case 3 -> {
+                    System.out.print("Enter Role (Manager/Regular/Intern): ");
+                    String role = sc.nextLine();
+                    System.out.print("Enter ID: ");
+                    String id = sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Department: ");
+                    String dept = sc.nextLine();
+                    System.out.print("Enter Gender: ");
+                    String gender = sc.nextLine();
+                    System.out.print("Enter Base Salary: ");
+                    double salary = sc.nextDouble();
+                    System.out.print("Enter Performance Rating: ");
+                    int rating = sc.nextInt();
+                    sc.nextLine();
+
+                    Employee emp = null;
+                    if (role.equalsIgnoreCase("Manager")) {
+                        System.out.print("Enter Team Size: ");
+                        int teamSize = sc.nextInt();
+                        sc.nextLine();
+                        System.out.print("Enter Project Name: ");
+                        String project = sc.nextLine();
+                        emp = new Manager(id, name, dept, gender, role, salary, rating, teamSize, project);
+                    } else if (role.equalsIgnoreCase("Regular")) {
+                        System.out.print("Enter Job Title: ");
+                        String title = sc.nextLine();
+                        System.out.print("Enter Years of Experience: ");
+                        int exp = sc.nextInt();
+                        sc.nextLine();
+                        emp = new RegularEmployee(id, name, dept, gender, role, salary, rating, title, exp);
+                    } else if (role.equalsIgnoreCase("Intern")) {
+                        System.out.print("Enter University Name: ");
+                        String uni = sc.nextLine();
+                        System.out.print("Enter Internship Duration (months): ");
+                        int duration = sc.nextInt();
+                        sc.nextLine();
+                        emp = new Intern(id, name, dept, gender, role, salary, rating, uni, duration);
+                    }
+                    if (emp != null) {
+                        employeeManager.addEmployee(emp);
+                        System.out.println(UIEnhancement.GREEN + "✔ Employee added successfully!" + UIEnhancement.RESET);
+                    }
+                }
+
+                case 4 -> {
+                    System.out.print("Enter name to search: ");
+                    String name = sc.nextLine();
+                    List<Employee> results = employeeManager.searchByName(name);
+                    if (results != null && !results.isEmpty()) {
+                        UIEnhancement.progressBar("Searching Employee by Name: ");
+                        for (Employee employee : results) {
+                            employee.displayEmployeeDetails();
+                        }
+                        System.out.println(UIEnhancement.GREEN+"-----------------------------------------------------");
+                        System.out.println("Total " + results.size() + " result(s) found for " + "'"+ name +"'");
+                        System.out.println("-----------------------------------------------------"+UIEnhancement.RESET);
+                    } else {
+                        System.out.println(UIEnhancement.RED + "No employees found with that name." + UIEnhancement.RESET);
+                    }
+                }
+
+                case 5 -> {
+                    System.out.print("Enter minimum performance rating: ");
+                    int rating = sc.nextInt();
+                    sc.nextLine();
+                    List<Employee> results = employeeManager.searchByPerformanceRating(rating);
+                    if (results != null && !results.isEmpty()) {
+                        UIEnhancement.progressBar("Searching Employee by Performance Rating: ");
+
+                        for (Employee e : results) e.displayEmployeeDetails();
+                        System.out.println(UIEnhancement.GREEN+"-----------------------------------------------------");
+                        System.out.println( "Total " + results.size() + " result(s) found for " + "'"+ rating +"'" );
+                        System.out.println("-----------------------------------------------------"+UIEnhancement.RESET);
+                    } else {
+                        System.out.println(UIEnhancement.RED + "No employees found with that rating or higher." + UIEnhancement.RESET);
+                    }
+                }
+
+                case 6 -> {
+                    System.out.print("Enter ID to search: ");
+                    String id = sc.nextLine();
+                    employeeManager.searchById(id);
+
+                }
+
+                case 7 -> {
+                    System.out.print("Enter ID: ");
+                    String id = sc.nextLine();
+                    employeeManager.updateEmployee(id, sc);
+                }
+
+                case 8 -> {
+                    System.out.print("Enter ID to delete: ");
+                    String id = sc.nextLine();
+                    employeeManager.deleteEmployee(id);
+                }
+
+                case 9 -> employeeManager.recentActions();
+
+                case 10 ->
+                        {
+                            employeeManager.saveToFile(filePath);
+                        }
+
+                case 11 -> System.out.println(UIEnhancement.GREEN + "\n👋 Exiting the system. Goodbye!" + UIEnhancement.RESET);
+            }
+
+        } while (choice != 11);
+        sc.close();
+    }
+}
---
+++ src/UIEnhancement.java
@@ -0,0 +1,24 @@
+public class UIEnhancement {
+    public static void progressBar(String message) {
+        System.out.print(message + " [");
+        for (int i = 0; i <= 20; i++) {
+            try {
+                Thread.sleep(40); // Speed of progress
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.print(GREEN + "█" + RESET);
+
+        }
+        System.out.println("] Done!");
+    }
+
+
+    // ANSI Colors
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String RED = "\u001B[31m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String YELLOW = "\u001B[33m";
+
+}
